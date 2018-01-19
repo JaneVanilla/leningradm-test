35ff3eb365ffabe2c469adb89cc498f0c57dd295
Zhanna Sydorovych <janevanilla92@gmail.com>
2018-01-19 15:58:23 +0300

    all files

---
+++ markup/components/_template/data/data.js
@@ -0,0 +1,19 @@
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+}
+
+   
---
+++ markup/components/form/data/data.js
@@ -0,0 +1,9 @@
+var data = {form: {
+	  _class: 'form',
+
+	  title: 'Please login',
+	  placeholder1: 'Your email',
+	  placeholder2: 'Your password',
+	  inputTitle: 'Remember me'
+
+}};
